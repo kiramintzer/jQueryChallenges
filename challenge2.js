a2bc688baf5eddb241697ea8b078dff8e2bfe500
kiramintzer <38299577+kiramintzer@users.noreply.github.com>
2019-11-23 16:10:48 -0500

    reset to blank after unchecking the box

---
+++ challenge2.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){ 
+	$("#useBilling").click(function(){
+		console.log("home address same as billing")
+		if ($("#useBilling").is(":checked")) {
+			$("#home").val($("#billing").val());
+			$("#home").attr("disabled", "disabled");
+		} 
+		else{
+			$("#home").removeAttr("disabled");
+			$("#home").val("");
+		}
+	})
+})
+
+
+// https://codepen.io/rubywahoo/pen/dmsBk?editors=1010
+
+// remember: attr takes two args in ( )
